e7f34f0d3c5ad777b2d996b29200c22ee23ed79b
Justin <justinheyns@gmail.com>
2019-10-08 07:48:51 +0200

    Added discord pixel

--- client/src/components/LayoutFrame/Social/index.tsx
+++ client/src/components/LayoutFrame/Social/index.tsx
@@ -2,12 +2,18 @@ import * as React from 'react'
 
 import github from '../../../customIcons/github.svg'
 import medium from '../../../customIcons/medium.svg'
+import discord from '../../../customIcons/discord.png'
 import { IconButton, Icon, withStyles } from '@material-ui/core';
 
 const style = (theme: any) => ({
 	Button: {
 		margin: "10px 20px 10px 10px",
 		padding: "0"
+	},
+	discord:{
+		padding: "0",
+		width:"25px",
+		margin: "0 auto"
 	}
 })
 
@@ -24,6 +30,9 @@ function SocialComponent(props:SocialProps){
 				</IconButton>
 				<IconButton className={classes.Button} onClick={()=>window.open('https://medium.com/weidaithriftcoin')}>
 					<Icon><img src={medium} /></Icon>
+				</IconButton>
+				<IconButton  onClick={()=>window.open('https://discord.gg/s6bKYjj')}>
+					<Icon><img className={classes.discord} src={discord} /></Icon>
 				</IconButton>
 			</div>
 		)
