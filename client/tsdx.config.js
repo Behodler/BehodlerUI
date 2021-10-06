37712ee35b77ba1e5027b430ae9583dddc52a8dd
michal.kloczko <michal.kloczko@tratif.com>
2021-10-06 21:45:54 +0200

    update app config, define package build config, install required dependencies

---
+++ client/tsdx.config.js
@@ -0,0 +1,31 @@
+const postcss = require('rollup-plugin-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+// const image = require('@rollup/plugin-image');
+const images = require('./src/util/rollup-plugin-image-files');
+const url = require('@rollup/plugin-url');
+const svgr = require('@svgr/rollup').default;
+
+module.exports = {
+    rollup(config, options) {
+        config.plugins = [
+            images(),
+            url({include: ['**/*.webp', '**/*.otf']}),
+            svgr(),
+            postcss({
+                plugins: [
+                    autoprefixer(),
+                    cssnano({
+                        preset: 'default',
+                    }),
+                ],
+                inject: false,
+                // only write out CSS for the first bundle (avoids pointless extra files):
+                extract: !!options.writeMeta,
+            }),
+            ...config.plugins,
+        ]
+
+        return config;
+    },
+};
