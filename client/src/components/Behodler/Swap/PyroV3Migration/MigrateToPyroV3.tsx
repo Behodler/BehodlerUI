b56a8601cdea08bd0a92d195390dcf17486cb535
michal.kloczko <michal.kloczko@tratif.com>
2021-11-25 14:02:09 +0100

    update colors and element sizing

--- client/src/components/Behodler/Swap/PyroV3Migration/MigrateToPyroV3.tsx
+++ client/src/components/Behodler/Swap/PyroV3Migration/MigrateToPyroV3.tsx
@@ -11,36 +11,37 @@ const StyledMigrateToPyroV3Wrapper = styled.div`
 
 const StyledMigrateToPyroV3Box = styled.div`
   align-items: center;
-  background-color: rgba(27, 14, 70, 0.4);
-  border: 1px solid #3d266d;
+  background-color: rgba(54, 12, 87, 0.24);
+  border: 1px solid #3e236c;
   border-radius: 8px;
-  color: #8072bd;
+  color: #9081d2;
   display: flex;
   justify-content: space-between;
-  font-size: 14px;
+  font-size: 13px;
   padding: 12px 24px;
 `
 
 const StyledMigrateToPyroV3Button = styled.button`
   align-items: center;
   background-color: transparent;
-  border: 1px solid #625499;
+  border: 1px solid #9081d2;
   border-radius: 4px;
-  color: #8072bd;
+  color: #9081d2;
   display: flex;
   justify-content: center;
+  font-size: 12px;
   padding: 6px 32px 4px;
   transition: all 0.2s ease;
 
   &:hover {
-    background-color: rgba(128, 114, 189, 0.6);
-    color: #180b3b;
+    background-color: #9081d2;
+    color: #25174f;
     cursor: pointer;
   }
 `
 
 const StyledMigrateToPyroV3Message = styled.span`
-  margin: 0 20px;
+  margin: 0 24px;
 `
 
 const StyledWarningIcon = styled(WarningIcon)`
