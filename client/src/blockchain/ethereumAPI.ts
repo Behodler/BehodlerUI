b77a2f363e853f1797a26496671e3ed1ed051e04
michal.kloczko <michal.kloczko@tratif.com>
2023-02-17 17:52:53 +0100

    cleanup - removed some unused stuff

--- client/src/blockchain/ethereumAPI.ts
+++ client/src/blockchain/ethereumAPI.ts
@@ -8,14 +8,10 @@ import IContracts, { BehodlerContracts, DefaultBehodlerContracts } from './ICont
 import { ERC20 } from './contractInterfaces/ERC20'
 import { Pyrotoken } from './contractInterfaces/behodler2/Pyrotoken'
 import { Weth } from './contractInterfaces/behodler/Weth'
-import { address } from './contractInterfaces/SolidityTypes'
 import { ERC20Effects } from './observables/ERC20'
 import { EtherEffects } from './observables/Ether'
 import Token from './observables/Token'
-import { PatienceRegulationEffects } from './observables/PatienceRegulationEngine'
-import { BankEffects } from './observables/WeiDaiBank'
 import ERC20JSON from './behodlerUI/ERC20.json'
-import { PyrotokenEffects } from './observables/PyrotokenEffects'
 import BehodlerContractMappings from '../temp/BehodlerABIAddressMapping.json'
 import Behodler2ContractMappings from '../blockchain/behodler2UI/Behodler.json'
 import Lachesis2Json from '../blockchain/behodler2UI/Lachesis.json'
@@ -26,33 +22,11 @@ import { Behodler2 } from './contractInterfaces/behodler2/Behodler2'
 import Behodler2Addresses from './behodler2UI/Addresses.json'
 import { Lachesis as Lachesis2 } from './contractInterfaces/behodler2/Lachesis'
 import { LiquidityReceiver } from './contractInterfaces/behodler2/LiquidityReceiver'
-import { BridgeEffects } from './observables/BridgeEffects'
-import { SluiceGateEffects } from './observables/SluiceGateEffects'
-import { LiquidQueueEffects } from './observables/LiquidQueueEffects'
 import Weth10JSON from './behodler2UI/WETH10.json'
 import PyrotokenJSON from './behodler2UI/Pyrotoken.json'
-import UniswapV2FactoryJSON from './liquidQueue/UniswapV2Factory.json'
-import LiquidQueueAddresses from './liquidQueue/Addresses.json'
-import UniswapV2Factory from './contractInterfaces/liquidQueue/UniswapV2Factory'
-import { UniswapV2Effects } from './observables/UniswapEffects'
 import { PyroWeth10Proxy } from './contractInterfaces/behodler2/PyroWeth10Proxy'
 import { TokenProxyRegistry } from "./contractInterfaces/limbo/TokenProxyRegistry";
 import { V2Migrator } from "./contractInterfaces/pyrotokens/V2Migrator";
-
-interface AccountObservable {
-    account: string
-    isPrimary: boolean
-    enabled: boolean
-    versionBalances: userWeiDaiBalances[]
-    oldBalances: boolean
-}
-
-export interface userWeiDaiBalances {
-    version: string
-    incubating: string
-    actual: string
-    enabled: boolean
-}
 
 interface newContracts {
     weiDai: string
@@ -60,35 +34,16 @@ interface newContracts {
     PRE: string
 }
 
-interface LQInputAddressList {
-    Dai: string
-    Eth: string
-    Scarcity: string
-    Eye: string
-}
-
 class ethereumAPI {
-    private interval: any
+    private newContracts: newContracts = { weiDai: '', weiDaiBank: '', PRE: '' }
 
-    private newContracts: newContracts
     public initialized: boolean
     public newContractObservable: Observable<newContracts>
     public networkMapping: { [key: number]: string }
     public contractsAvailable: string[]
     public web3: Web3
-    public accountObservable: Observable<AccountObservable>
-    public weiDaiEffects: ERC20Effects
-    public daiEffects: ERC20Effects
-    public preEffects: PatienceRegulationEffects
-    public bankEffects: BankEffects
     public scarcityEffects: ERC20Effects
-    public bridgeEffects: BridgeEffects
-    public sluiceGateEffects: SluiceGateEffects
-    public liquidQueueEffects: LiquidQueueEffects
-    public eyeWethLPEffects: UniswapV2Effects
-    public scxEyeLPEffects: UniswapV2Effects
     public UINTMAX: string = '115792089237316000000000000000000000000000000000000000000000000000000000000000'
-    public MAXETH: string = '115792089237316000000000000000000000000000000000000000000000'
     public ONE = BigInt('1000000000000000000')
     constructor() {
         this.networkMapping = {
@@ -103,7 +58,6 @@ class ethereumAPI {
         }
         // if we want to add more chains, add their ids and names in this.networkMapping and add their names to the array below
         this.contractsAvailable = ['main']
-        this.newContracts = { weiDai: '', weiDaiBank: '', PRE: '' }
     }
 
     public async initialize(chainId, currentAccount: string): Promise<IContracts> {
@@ -123,10 +77,6 @@ class ethereumAPI {
         await this.setupSubscriptions()
 
         return contracts
-    }
-
-    public toBytes(input: string) {
-        return this.web3.utils.fromAscii(input)
     }
 
     public addBlockWatcher(watcher: (b: string) => void) {
@@ -166,14 +116,6 @@ class ethereumAPI {
         }
     }
 
-    public unsubscribeAccount() {
-        clearInterval(this.interval)
-    }
-
-    public hexToNumberString(value: any): string {
-        return this.web3.utils.hexToNumberString(value['_hex'])
-    }
-
     public async enableToken(tokenAddress: string, owner: string, spender: string, callBack?: (err: any, hash: string) => void): Promise<void> {
         const token: ERC20 = new this.web3.eth.Contract(ERC20JSON.abi as any, tokenAddress).methods as unknown as ERC20
         await token
@@ -182,18 +124,10 @@ class ethereumAPI {
             .catch((err) => console.log('user rejected'))
     }
 
-    public isEth(token: string, network: string): boolean {
-        return LiquidQueueAddresses[network].WETH === token
-    }
     public async getToken(tokenAddress: string, network: string): Promise<ERC20> {
         var token = new this.web3.eth.Contract(ERC20JSON.abi as any, tokenAddress).methods as unknown as ERC20
         token.address = tokenAddress
         return token
-    }
-
-    public getEYEAddress(network: string): string {
-        const EYE = LiquidQueueAddresses[network].EYE
-        return EYE
     }
 
     public async getPyroToken(tokenAddress: string, network: string): Promise<Pyrotoken> {
@@ -209,129 +143,8 @@ class ethereumAPI {
         return new ERC20Effects(this.web3, token, currentAccount, decimalPlaces)
     }
 
-    public async getPyroTokenEffects(pyroTokenAddress: string, network: string, account: string): Promise<PyrotokenEffects> {
-        const pyroTokenInstance = await this.getPyroToken(pyroTokenAddress, network)
-        return new PyrotokenEffects(this.web3, pyroTokenInstance, account)
-    }
-
-    public async EYE_ETH_PAIR(network: address, account: string): Promise<string> {
-        const EYE = LiquidQueueAddresses[network].EYE
-        const WETH = LiquidQueueAddresses[network].WETH
-        const factoryInstance: UniswapV2Factory = (await this.deployBehodlerContract(UniswapV2FactoryJSON.abi, LiquidQueueAddresses[network].UniswapV2Factory)).methods
-        return await factoryInstance.getPair(EYE, WETH).call({ from: account })
-    }
-
-    public async EYE_SCX_PAIR(network: address, account: string): Promise<string> {
-        const EYE = LiquidQueueAddresses[network].EYE
-        const SCX = LiquidQueueAddresses[network].SCX
-        const factoryInstance: UniswapV2Factory = (await this.deployBehodlerContract(UniswapV2FactoryJSON.abi, LiquidQueueAddresses[network].UniswapV2Factory)).methods
-        return await factoryInstance.getPair(EYE, SCX).call({ from: account })
-    }
-    public async EYE_ETHEffects(network: address, account: string): Promise<Token> {
-        const pair = await this.EYE_ETH_PAIR(network, account)
-        return this.generateNewEffects(pair, account, false)
-    }
-
-    public async EYE_SCXEffects(network: address, account: string): Promise<Token> {
-        const pair = await this.EYE_SCX_PAIR(network, account)
-        return this.generateNewEffects(pair, account, false)
-    }
-
-    public getLQInputAddresses(network: string): LQInputAddressList {
-        return {
-            Dai: LiquidQueueAddresses[network].DAI,
-            Eth: LiquidQueueAddresses[network].WETH,
-            Scarcity: LiquidQueueAddresses[network].SCX,
-            Eye: LiquidQueueAddresses[network].EYE,
-        }
-    }
-
     public async getEthBalance(account: string): Promise<string> {
         return await this.web3.eth.getBalance(account)
-    }
-
-    public constructRewardToken(tokenAddress: string, network: string) {
-        switch (tokenAddress) {
-            case LiquidQueueAddresses[network].DAI:
-                return 'EYE/DAI'
-            case LiquidQueueAddresses[network].WETH:
-                return 'SCX/WETH'
-            case LiquidQueueAddresses[network].SCX:
-                return 'SCX/EYE'
-            case LiquidQueueAddresses[network].EYE:
-                return 'SCX/EYE'
-            default:
-                return 'not found'
-        }
-    }
-
-    public async getTokenBalanceBig(tokenAddress: string, currentAccount: string, isEth: boolean, decimalPlaces: number): Promise<any> {
-        if (isEth) {
-            return (await this.web3.eth.getBalance(currentAccount))
-        }
-        const token: ERC20 = new this.web3.eth.Contract(ERC20JSON.abi as any, tokenAddress).methods as unknown as ERC20
-        const balance = (await token.balanceOf(currentAccount).call({ from: currentAccount }))
-        return balance
-    }
-
-    public async getTokenBalance(tokenAddress: string, currentAccount: string, isEth: boolean, decimalPlaces: number): Promise<string> {
-        return await this.getTokenBalanceBig(tokenAddress,currentAccount,isEth,decimalPlaces)
-    }
-
-    public async getTokenSymbol(tokenAddress: string): Promise<string> {
-        const token: ERC20 = (await (new this.web3.eth.Contract(PyrotokenJSON.abi as any, tokenAddress).methods as unknown)) as ERC20
-        return await token.symbol().call()
-    }
-
-    public async getTokenDecimals(tokenAddress: string): Promise<number> {
-        const token: ERC20 = new this.web3.eth.Contract(ERC20JSON.abi as any, tokenAddress).methods as unknown as ERC20
-        try {
-            return parseInt((await token.decimals().call()).toString())
-        } catch {
-            return 18
-        }
-    }
-
-    public async getTokenTotalSupply(tokenAddress: string): Promise<string> {
-        const token: ERC20 = new this.web3.eth.Contract(ERC20JSON.abi as any, tokenAddress).methods as unknown as ERC20
-        try {
-            return (await token.totalSupply().call()).toString()
-        } catch {
-            return '0'
-        }
-    }
-
-    public async getTokenAllowance(tokenAddress: string, currentAccount: string, isEth: boolean, decimalPlaces: number, behodlerAddress: string): Promise<BigNumber> {
-        BigNumber.set({ POW_PRECISION: 100 })
-        BigNumber.config({ EXPONENTIAL_AT: 100 })
-        const token: ERC20 = new this.web3.eth.Contract(ERC20JSON.abi as any, tokenAddress).methods as unknown as ERC20
-        const allowance = await token.allowance(currentAccount, behodlerAddress).call({ from: currentAccount })
-
-        return new BigNumber(allowance)
-    }
-    public hexToNumber(value: any): number {
-        return parseFloat(this.hexToNumberString(value))
-    }
-
-    // public pureHexToNumberString(value: any): string {
-    //     console.log('received value to convert to hex ' + value)
-    //     if (value === '0') return '0'
-    //     try {
-    //         var result = this.web3.utils.hexToNumberString(value)
-    //         console.log('converting to hex ' + value + ' gives ' + result)
-    //         return result
-    //     } catch (err) {
-    //         return value
-    //     }
-    // }
-
-    public pureHexToNumber(value: any): number {
-        try {
-            return this.web3.utils.hexToNumber(value)
-        } catch (err) {
-            console.log('error on conversion ' + err)
-            return value
-        }
     }
 
     private async setupSubscriptions(): Promise<void> {
