fd5881150934804a63e44b93ed0a76d7a2ecfd66
michal.kloczko <michal.kloczko@tratif.com>
2023-03-02 16:20:50 +0100

    just test smth

--- client/src/components/Behodler/Swap/PyroV3Swap/PyroV3Swap.tsx
+++ client/src/components/Behodler/Swap/PyroV3Swap/PyroV3Swap.tsx
@@ -1,40 +1,9 @@
 import React from 'react'
 
-import { usePyroV3SwapState, useSetMinting, useSetInsufficientBalance } from './hooks/usePyroV3SwapState'
-import { Tesssst } from './Tesssst'
+import { TradingUI } from './TradingUI/TradingUI'
 
 export function PyroV3Swap() {
-    const pyroV3SwapState = usePyroV3SwapState()
-    const setMinting = useSetMinting()
-    const setInsufficientBalance = useSetInsufficientBalance()
-
-    const toggleMinting = () => {
-        setMinting(!pyroV3SwapState.minting)
-    }
-
-    const toggleInsufficientBalance = () => {
-        setInsufficientBalance(!pyroV3SwapState.insufficientBalance)
-    }
-
-    console.info('render PyroV3Swap');
-
     return (
-        <div style={{ color: 'white' }}>
-            <pre>
-                {JSON.stringify(pyroV3SwapState)}
-            </pre>
-
-            <button onClick={toggleMinting}>
-                toggle minting
-            </button>
-
-            <button onClick={toggleInsufficientBalance}>
-                toggle insufficient balance
-            </button>
-
-            <br />
-
-            <Tesssst />
-        </div>
+        <TradingUI />
     )
 }
--- client/src/components/Behodler/Swap/index.tsx
+++ client/src/components/Behodler/Swap/index.tsx
@@ -1,24 +1,19 @@
 import * as React from 'react'
 
 import TradingBox3 from './TradingBox3'
-import { PyroV3Swap } from './PyroV3Swap'
 import { WalletContextProvider } from '../../Contexts/WalletStatusContext'
 import { StatelessBehodlerContextProvider } from './EVM_js/context/StatelessBehodlerContext'
 import backImage from "../../../images/new/behodler-swap-bg.jpg"
 
 import './fonts/gilroy-font.css'
-import useActiveWeb3React from "./hooks/useActiveWeb3React";
 
 export const pyrotokensBackgroundImage = backImage
 
 function Swap() {
-    const { chainId } = useActiveWeb3React()
-    const isDevEnv = chainId === 1337;
-
     try {
         return (
             <StatelessBehodlerContextProvider>
-                {!isDevEnv ? <PyroV3Swap /> : <TradingBox3 />}
+                <TradingBox3 />
             </StatelessBehodlerContextProvider>
         )
     } catch (e) {
